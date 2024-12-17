926a5c326bc2463edbe6acd3cfbb2519a0888e1a
Dragan <Dragan>
2024-12-17 19:46:38 +0100

    qq

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -17,7 +17,7 @@ for (int i = 0; i < 10; i++)
 
 
     // Invoke the method and get the Task result
-    Task task = (Task)method.Invoke(null, new object[] { "https://jsonplaceholder.typicode.com/todos/1" })!;
+    Task task = (Task)method.Invoke(null, ["https://jsonplaceholder.typicode.com/todos/1"])!;
     await task;
 
     // Get the Result property from Task<T>
