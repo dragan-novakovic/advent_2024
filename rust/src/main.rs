19f4cded24104872e4a29765bfb648f33c205bd0
Dragan <Dragan>
2024-12-18 11:50:08 +0100

    qqq

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,7 +4,7 @@ mod utils;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 1..1 {
-        let input = utils::get_task_input::<day1::task1::Input>(day1::task1::INTPUT_URL).await?;
+        let _input = utils::get_task_input::<day1::task1::Input>(day1::task1::INTPUT_URL).await?;
     }
     Ok(())
 }
--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,3 +1,15 @@
+use std::any::Any;
+
+fn print_any(value: &dyn Any) {
+    if let Some(string) = value.downcast_ref::<String>() {
+        println!("String value: {}", string);
+    } else if let Some(int) = value.downcast_ref::<i32>() {
+        println!("Integer value: {}", int);
+    } else {
+        println!("Unknown type");
+    }
+}
+
 trait TaskInput {
     async fn get_task_input() -> Result<Self, Box<dyn std::error::Error>>
     where
