1da8d57a10aa8a562fac814ffbb03105e84054e7
dragan1810 <d.novakovic1810@yahoo.com>
2026-06-13 21:26:00 +0200

    Update task1.rs

--- rust/src/advent2016/day1/task1.rs
+++ rust/src/advent2016/day1/task1.rs
@@ -6,21 +6,74 @@ pub enum Direction {
     Right,
 }
 
-pub type Coords = (Option<i32>, Option<i32>);
+pub enum Orientation {
+    Bei,
+    Nan,
+    Xi,
+    Dong,
+}
+
 pub type Move = (Direction, i32);
+
+#[derive(Debug)]
+struct Position {
+    bei: i32,
+    nan: i32,
+    xi: i32,
+    dong: i32,
+}
+
+impl Default for Position {
+    fn default() -> Self {
+        Self {
+            bei: 0,
+            nan: 0,
+            xi: 0,
+            dong: 0,
+        }
+    }
+}
 
 pub fn runner() {
     let input = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4";
-    let my_cords: Coords = (None, None);
-    let target: Coords = (None, None);
-
+    //  let demo = "R5, L5, R5, R3";
     let mvs = convert_input_to_directions(input);
-    do_moving(mvs);
+    let result = do_moving(mvs);
+    dbg!(result);
 }
 
-pub fn do_moving(mvs: Vec<Move>) {
+pub fn do_moving(mvs: Vec<Move>) -> i32 {
+    let mut position: Position = Position {
+        ..Position::default()
+    };
+    let mut current_orientation = Orientation::Bei;
     //update my cords + estimates
-    //
+    for (dir, steps) in mvs {
+        match dir {
+            Direction::Left => match current_orientation {
+                Orientation::Bei => current_orientation = Orientation::Xi,
+                Orientation::Nan => current_orientation = Orientation::Dong,
+                Orientation::Xi => current_orientation = Orientation::Nan,
+                Orientation::Dong => current_orientation = Orientation::Bei,
+            },
+
+            Direction::Right => match current_orientation {
+                Orientation::Bei => current_orientation = Orientation::Dong,
+                Orientation::Nan => current_orientation = Orientation::Xi,
+                Orientation::Xi => current_orientation = Orientation::Bei,
+                Orientation::Dong => current_orientation = Orientation::Nan,
+            },
+        }
+
+        match current_orientation {
+            Orientation::Bei => position.bei += steps,
+            Orientation::Nan => position.nan += steps,
+            Orientation::Xi => position.xi += steps,
+            Orientation::Dong => position.dong += steps,
+        }
+    }
+
+    (position.bei - position.nan).abs() + (position.dong - position.xi).abs()
 }
 
 pub fn convert_input_to_directions(list: &str) -> Vec<Move> {
@@ -35,8 +88,4 @@ pub fn convert_input_to_directions(list: &str) -> Vec<Move> {
         acc.push((direction, steps));
         acc
     })
-}
-
-pub fn get_block_distance() -> i32 {
-    2
 }
