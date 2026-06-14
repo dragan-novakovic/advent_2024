d12211bc8ffe17b6e5e64d7b48592ef768047a86
dragan1810 <d.novakovic1810@yahoo.com>
2026-06-14 08:34:32 +0200

    qq

--- rust/src/advent2016/day1/task1.rs
+++ rust/src/advent2016/day1/task1.rs
@@ -14,13 +14,15 @@ pub enum Orientation {
 }
 
 pub type Move = (Direction, i32);
+pub type Coord = (i32, i32);
 
 #[derive(Debug)]
-struct Position {
+pub struct Position {
     bei: i32,
     nan: i32,
     xi: i32,
     dong: i32,
+    coord_history: Vec<Coord>,
 }
 
 impl Default for Position {
@@ -30,22 +32,22 @@ impl Default for Position {
             nan: 0,
             xi: 0,
             dong: 0,
+            coord_history: vec![],
         }
     }
 }
 
 pub fn runner() {
     let input = "L4, L3, R1, L4, R2, R2, L1, L2, R1, R1, L3, R5, L2, R5, L4, L3, R2, R2, L5, L1, R4, L1, R3, L3, R5, R2, L5, R2, R1, R1, L5, R1, L3, L2, L5, R4, R4, L2, L1, L1, R1, R1, L185, R4, L1, L1, R5, R1, L1, L3, L2, L1, R2, R2, R2, L1, L1, R4, R5, R53, L1, R1, R78, R3, R4, L1, R5, L1, L4, R3, R3, L3, L3, R191, R4, R1, L4, L1, R3, L1, L2, R3, R2, R4, R5, R5, L3, L5, R2, R3, L1, L1, L3, R1, R4, R1, R3, R4, R4, R4, R5, R2, L5, R1, R2, R5, L3, L4, R1, L5, R1, L4, L3, R5, R5, L3, L4, L4, R2, R2, L5, R3, R1, R2, R5, L5, L3, R4, L5, R5, L3, R1, L1, R4, R4, L3, R2, R5, R1, R2, L1, R4, R1, L3, L3, L5, R2, R5, L1, L4, R3, R3, L3, R2, L5, R1, R3, L3, R2, L1, R4, R3, L4, R5, L2, L2, R5, R1, R2, L4, L4, L5, R3, L4";
-    //  let demo = "R5, L5, R5, R3";
-    let mvs = convert_input_to_directions(input);
-    let result = do_moving(mvs);
-    dbg!(result);
-}
-
-pub fn do_moving(mvs: Vec<Move>) -> i32 {
     let mut position: Position = Position {
         ..Position::default()
     };
+    let mvs = convert_input_to_directions(input);
+    let result = do_moving(position, mvs);
+    dbg!(result);
+}
+
+pub fn do_moving(mut pos: Position, mvs: Vec<Move>) -> i32 {
     let mut current_orientation = Orientation::Bei;
     //update my cords + estimates
     for (dir, steps) in mvs {
@@ -66,14 +68,14 @@ pub fn do_moving(mvs: Vec<Move>) -> i32 {
         }
 
         match current_orientation {
-            Orientation::Bei => position.bei += steps,
-            Orientation::Nan => position.nan += steps,
-            Orientation::Xi => position.xi += steps,
-            Orientation::Dong => position.dong += steps,
+            Orientation::Bei => pos.bei += steps,
+            Orientation::Nan => pos.nan += steps,
+            Orientation::Xi => pos.xi += steps,
+            Orientation::Dong => pos.dong += steps,
         }
     }
 
-    (position.bei - position.nan).abs() + (position.dong - position.xi).abs()
+    (pos.bei - pos.nan).abs() + (pos.dong - pos.xi).abs()
 }
 
 pub fn convert_input_to_directions(list: &str) -> Vec<Move> {
@@ -89,3 +91,13 @@ pub fn convert_input_to_directions(list: &str) -> Vec<Move> {
         acc
     })
 }
+
+/*
+* Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+
+How many blocks away is the first location you visit twice?
+*
+*
+*/
